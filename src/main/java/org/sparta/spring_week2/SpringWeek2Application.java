3667a55117920ed1bc6b56115df525857a0984f5
minnieming <eunmilee9109.1@gmail.com>
2024-02-26 15:08:52 +0900

    first commit

---
+++ src/main/java/org/sparta/spring_week2/SpringWeek2Application.java
@@ -0,0 +1,13 @@
+package org.sparta.spring_week2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeek2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeek2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/sparta/spring_week2/SpringWeek2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sparta.spring_week2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWeek2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
